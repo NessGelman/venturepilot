3a04357f26132415bdcc2c611451a8f2f0126d6e
NessGelman <NessGelman@users.noreply.github.com>
2026-03-25 22:00:03 +0000

    deploy: 318b06695536d696d3588e7a6e405355b6015752

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Self-destruct SW: clears all caches, unregisters itself, reloads all tabs
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', async () => {
-  const keys = await caches.keys();
-  await Promise.all(keys.map(k => caches.delete(k)));
-  await self.registration.unregister();
-  const clients = await self.clients.matchAll({ type: 'window' });
-  clients.forEach(c => c.navigate(c.url));
-});
